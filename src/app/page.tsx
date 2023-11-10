8f1c3b04b47b37ddb0a1eced92e5b7eae672ace8
Takuya Eguchi <seal2501@gmail.com>
2023-11-10 09:20:55 +0900

    Add @mui/x-date-pickers for reproduce button text shifted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+import { DateCalendar, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
 export default function Home() {
-  return <main>wip</main>;
+  return (
+    <main>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateCalendar />
+      </LocalizationProvider>
+    </main>
+  );
 }
